f6c4ccadba9fc4ee8b33ef36550e2b5e50925e10
Amodmathapati <103567778+Amodmathapati@users.noreply.github.com>
2025-05-28 12:32:27 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,59 +1,38 @@
-import userModel from "../models/userModel.js";
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import userModel from './models/userModel.js';
 
-export const registerController = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  //validate
-  if (!name) {
-    next("name is required");
+passport.use(new LocalStrategy({
+  usernameField: 'email',  // by default, it's 'username', so we override here
+  passwordField: 'password',
+}, async (email, password, done) => {
+  try {
+    const user = await userModel.findOne({ email }).select('+password');
+    if (!user) {
+      return done(null, false, { message: 'Incorrect email or password.' });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return done(null, false, { message: 'Incorrect email or password.' });
+    }
+    user.password = undefined;
+    return done(null, user);
+  } catch (error) {
+    return done(error);
   }
-  if (!email) {
-    next("email is required");
-  }
-  if (!password) {
-    next("password is required and greater than 6 character");
-  }
-  const exisitingUser = await userModel.findOne({ email });
-  if (exisitingUser) {
-    next("Email Already Register Please Login");
-  }
-  const user = await userModel.create({ name, email, password });
-  //token
-  const token = user.createJWT();
-  res.status(201).send({
-    sucess: true,
-    message: "User Created Successfully",
-    user: {
-      name: user.name,
-      lastName: user.lastName,
-      email: user.email,
-      location: user.location,
-    },
-    token,
-  });
-};
+}));
 
-export const loginController = async (req, res, next) => {
-  const { email, password } = req.body;
-  //validation
-  if (!email || !password) {
-    next("Please Provide All Fields");
+// Serialize user to store in session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize user from session
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await userModel.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
   }
-  //find user by email
-  const user = await userModel.findOne({ email }).select("+password");
-  if (!user) {
-    next("Invalid Useraname or password");
-  }
-  //compare password
-  const isMatch = await user.comparePassword(password);
-  if (!isMatch) {
-    next("Invalid Useraname or password");
-  }
-  user.password = undefined;
-  const token = user.createJWT();
-  res.status(200).json({
-    success: true,
-    message: "Login SUccessfully",
-    user,
-    token,
-  });
-};
+});
